3d698055d02068fd88ac024e8edcb9c7dc06e0db
Andrew <andrewsaffar@free.fr>
2020-06-26 16:37:08 +0200

    added description to post list

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    description = models.CharField(max_length=400)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
